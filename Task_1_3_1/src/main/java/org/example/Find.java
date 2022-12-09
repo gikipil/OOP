edb3633baef84f0a4bb1658d661cf00c55e58c1a
gikipil <frostcool1998@gmail.com>
2022-12-09 21:34:04 +0700

    fix rewiedog2

--- Task_1_3_1/src/main/java/org/example/Find.java
+++ Task_1_3_1/src/main/java/org/example/Find.java
@@ -1,10 +1,10 @@
 package org.example;
 
 
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
@@ -80,7 +80,7 @@ public class Find {
      * @return returns the index of the beginning of the substring.
      */
 
-     private int find_math(String text, String example) {
+    private int find_math(String text, String example) {
         int math = 0;
         for (int i = 0; i < text.length(); i++) {
 
