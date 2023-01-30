960a9126ac844ca70862068a231b65acdaec52b3
gikipil <frostcool1998@gmail.com>
2023-01-31 01:42:49 +0700

    first

---
+++ Task_1_2_3/src/main/java/org/example/Graf.java
@@ -0,0 +1,401 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * The structure of the vertex and the length of the incoming edge.
+ */
+
+class vert<T> {
+    T val;
+    Integer weight;
+    public vert(T val, Integer weight) {
+        this.val = val;
+        this.weight = weight;
+    }
+    public String toString() {
+        return this.val + " (" + this.weight + ")";
+    }
+}
+
+/**
+ * Graph class.
+ */
+
+public class Graf<T> {
+
+    /**
+     * The number of vertices.
+     */
+
+    int count = 0;
+
+    /**
+     * Hashmap of the vertex and its indexer in the list.
+     */
+
+    HashMap<T, Integer> vertList = new HashMap<>();
+
+    /**
+     * The adjacency list.
+     */
+
+    List<ArrayList<vert<T>>> adjacency = new ArrayList<ArrayList<vert<T>>>();
+
+    /**
+     * Method of adding a vertex.
+     *
+     * @param vert the vertex to add.
+     */
+
+    public void addVert(T vert) {
+        vertList.put(vert, count);
+        count++;
+        adjacency.add(vertList.get(vert), new ArrayList<>());
+    }
+
+    /**
+     * The method of adding an edge.
+     *
+     * @param from The vertex from which the edge comes.
+     *
+     * @param to The vertex to which the edge leads.
+     *
+     * @param weight the weight of the vertex.
+     */
+
+    public void addEdge(T from, T to, Integer weight) {
+        vert<T> temp = new vert<>(to, weight);
+        adjacency.get(vertList.get(from)).add(temp);
+    }
+
+    /**
+     * Method of deleting a vertex.
+     *
+     * @param vert the vertex to be deleted.
+     */
+
+    public void removeVert(T vert) {
+        vertList.remove(vert);
+        for (ArrayList<org.example.vert<T>> verts : adjacency) {
+            verts.removeIf(ex -> ex.val == vert);
+        }
+    }
+
+    /**
+     * The method of removing an edge.
+     *
+     * @param from the vertex from which the edge comes.
+     *
+     * @param to the vertex to which the edge leads.
+     */
+
+    public void removeEdge(T from, T to) {
+        adjacency.get(vertList.get(from)).removeIf(ex -> ex.val == to);
+    }
+
+    /**
+     * Method for obtaining the edge weight.
+     *
+     * @param from the vertex from which the edge comes.
+     *
+     * @param to the vertex to which the edge leads.
+     *
+     * @return weight.
+     */
+
+    public Integer getWeightEdge(T from, T to){
+        for (vert<T> i : adjacency.get(vertList.get(from))) {
+            if (i.val == to) {
+                return i.weight;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * A method for obtaining the edge weight by indexes.
+     *
+     * @param from the vertex from which the edge comes.
+     *
+     * @param to the vertex to which the edge leads.
+     *
+     * @return weight.
+     */
+
+    public Integer getWeightEdgeForIndex(int from, int to){
+        T toVal = null;
+        for (Map.Entry<T, Integer> entry : vertList.entrySet()) {
+            if (Objects.equals(to, entry.getValue())) {
+                toVal = entry.getKey();
+            }
+        }
+        for (vert<T> i : adjacency.get(from)) {
+            if (i.val == toVal) {
+                return i.weight;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * The method of changing the edge weight.
+     *
+     * @param from the vertex from which the edge comes.
+     *
+     * @param to the vertex to which the edge leads.
+     *
+     * @param val New weight.
+     */
+
+    public void setWeightEdge(T from, T to, Integer val) {
+        for (vert<T> i : adjacency.get(vertList.get(from))) {
+            if (i.val == to) {
+                i.weight = val;
+            }
+        }
+    }
+
+    /**
+     * Method for getting a list of vertices.
+     *
+     * @return list of vertices.
+     */
+
+    public ArrayList<T> getAllVert() {
+        return new ArrayList<>(vertList.keySet());
+    }
+
+    /**
+     * Method for getting a list of vertices adjacent to a given one.
+     *
+     * @param from the vertex from which the edge comes.
+     *
+     * @return a list of adjacent vertices.
+     */
+
+    public ArrayList<T> getEdges(T from) {
+        ArrayList<T> ans = new ArrayList<>();
+        for (vert<T> i : adjacency.get(vertList.get(from))) {
+            ans.add(i.val);
+        }
+        return ans;
+    }
+
+    /**
+     * Vertex change method.
+     *
+     * @param before the vertex that needs to be changed.
+     *
+     * @param after a new vert.
+     */
+
+    public  void setVert (T before, T after) {
+        vertList.put(after,vertList.get(before));
+        vertList.remove(before);
+        for (vert<T> i : adjacency.get(vertList.get(after))) {
+            i.val = after;
+        }
+    }
+
+    /**
+     * A method that returns an adjacency matrix.
+     */
+
+    public ArrayList<ArrayList<Integer>> adjMatrix() {
+        int x = 0;
+        int y = 0;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<T> v = this.getAllVert();
+        for (T i : v) {
+            ans.add(x, new ArrayList<>());
+            y = 0;
+            for (T j: v) {
+                ans.get(x).add(y, getWeightEdge(i, j));
+                y++;
+            }
+            x++;
+        }
+        return ans;
+    }
+
+    /**
+     *A method that returns the incidence matrix.
+     */
+
+    public ArrayList<ArrayList<Integer>> incMatrix() {
+        int x = 0;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<T> v = this.getAllVert();
+        for (T i : v) {
+            ans.add(x, new ArrayList<>());
+            int y = 0;
+            for (T j : v) {
+                for (vert<T> l: adjacency.get(vertList.get(j))) {
+                    if (i == l.val || i == j) {
+                        ans.get(x).add(y, l.weight);
+                        y++;
+                    } else {
+                        ans.get(x).add(y, null);
+                        y++;
+                    }
+                }
+            }
+            x++;
+        }
+        return ans;
+    }
+
+    /**
+     * A method that returns an adjacency list.
+     */
+
+    public ArrayList<ArrayList<T>> adjList() {
+        int x = 0;
+        int y = 0;
+        ArrayList<ArrayList<T>> ans = new ArrayList<>();
+        ArrayList<T> v = this.getAllVert();
+        for (T i : v) {
+            ans.add(x, new ArrayList<>());
+            y = 0;
+            for (T j: v) {
+                if (getWeightEdge(i, j) != null) {
+                    ans.get(x).add(y, j);
+                    y++;
+                }
+            }
+            x++;
+        }
+        return ans;
+    }
+
+    /**
+     * A method that checks whether an array contains a value.
+     *
+     * @param arr the array.
+     * @return true or false.
+     */
+
+    private boolean contains(int[] arr) {
+        for (int element : arr) {
+            if (element == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * A method that finds the minimum incomplete vertex.
+     *
+     * @param arr array of vertices.
+     *
+     * @param mark array with attendance marks.
+     *
+     * @return minimum path value.
+     */
+
+    private Integer min(Integer [] arr, int [] mark) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min && mark[i] == 1) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * A method that determines the next vertex.
+     *
+     * @param mark array with attendance marks.
+     *
+     * @param dist array of vertices.
+     *
+     * @return next vertex.
+     */
+
+    private int nextVert(int [] mark, Integer [] dist) {
+        for (int i = 0; i < dist.length; i++) {
+            if (mark[i] == 1 && Objects.equals(dist[i], min(dist, mark))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * A method that finds the minimum path from a given vertex using Dijkstra's algorithm.
+     *
+     * @param start start vertex.
+     *
+     * @return An array with minimal paths.
+     */
+
+    public Integer [] deikstra(T start) {
+        int max = Integer.MAX_VALUE;
+        int len = this.count;
+        Integer [] dist = new Integer[len];
+        int [] mark = new int[len];
+        for (int i = 0; i < len; i++) {
+            dist[i] = max;
+            mark[i] = 0;
+        }
+        int s = vertList.get(start);
+        dist[s] = 0;
+        mark[s] = 1;
+        while (contains(mark)) {
+            int u = nextVert(mark,dist);
+            if (u == -1) {
+                break;
+            }
+            mark[u] = 2;
+            for (int j = 0; j < len; ++j) {
+                Integer tempWeight = this.getWeightEdgeForIndex(u, j);
+                if (tempWeight != null) {
+                    if (dist[u] + tempWeight < dist[j]) {
+                        dist[j] = dist[u] + tempWeight;
+                        mark[j] = 1;
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+
+    /**
+     * A method for sorting vertices relative to the minimum path from the specified one.
+     *
+     * @param st start vertex.
+     *
+     * @return sorted list.
+     */
+
+    public ArrayList<T> sortVert(T st) {
+        ArrayList<T> list = getAllVert();
+        Integer [] data = deikstra(st);
+        int len = this.count;
+        for (int i = 0; i < len - 1; i++) {
+            for(int j = 0; j < len - i - 1; j++) {
+                if(data[j + 1] < data[j]) {
+                    int swap = data[j];
+                    Collections.swap(list, j, j+1);
+                    data[j] = data[j + 1];
+                    data[j + 1] = swap;
+
+                }
+            }
+        }
+        return list;
+    }
+
+
+
+}
---
+++ Task_1_2_3/src/test/java/GrafTest.java
@@ -0,0 +1,236 @@
+import org.example.Graf;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GrafTest {
+
+    /**
+     * Testing remove vert.
+     */
+
+    @Test
+    void removeVertTest() {
+        Graf<Integer> obj1 = new Graf<>();
+        obj1.addVert(1);
+        obj1.addVert(2);
+        obj1.addVert(3);
+        obj1.addVert(4);
+        obj1.addVert(5);
+        obj1.removeVert(3);
+        Graf<Integer> obj = new Graf<>();
+        obj.addVert(1);
+        obj.addVert(2);
+        obj.addVert(4);
+        obj.addVert(5);
+        Assertions.assertEquals(obj1.adjList(), obj.adjList());
+    }
+
+    /**
+     * Testing remove edge.
+     */
+
+    @Test
+    void removeEdgeTest() {
+        Graf<Integer> obj1 = new Graf<>();
+        obj1.addVert(1);
+        obj1.addVert(2);
+        obj1.addVert(3);
+        obj1.addEdge(1, 2, 3);
+        obj1.addEdge(1, 3, 6);
+        obj1.addEdge(2, 3, 1);
+        obj1.removeEdge(1, 2);
+        Graf<Integer> obj = new Graf<>();
+        obj.addVert(1);
+        obj.addVert(2);
+        obj.addVert(3);
+        obj.addEdge(1, 3, 6);
+        obj.addEdge(2, 3, 1);
+        Assertions.assertEquals(obj1.adjList(), obj.adjList());
+    }
+
+    /**
+     * Testing getWeightEdge.
+     */
+
+    @Test
+    void getWeightEdgeTest() {
+        Graf<Integer> obj1 = new Graf<>();
+        obj1.addVert(1);
+        obj1.addVert(2);
+        obj1.addVert(3);
+        obj1.addEdge(1, 2, 3);
+        obj1.addEdge(1, 3, 6);
+        Integer weight = obj1.getWeightEdge(1, 3);
+        Assertions.assertEquals(weight, 6);
+    }
+
+    /**
+     * Testing getWeightEdgeForIndex.
+     */
+
+    @Test
+    void getWeightEdgeForIndexTest() {
+        Graf<Integer> obj1 = new Graf<>();
+        obj1.addVert(1);
+        obj1.addVert(2);
+        obj1.addVert(3);
+        obj1.addEdge(1, 2, 3);
+        obj1.addEdge(1, 3, 6);
+        Integer weight = obj1.getWeightEdgeForIndex(0, 1);
+        Assertions.assertEquals(weight, 3);
+    }
+
+    /**
+     * Testing setWeightEdge.
+     */
+
+    @Test
+    void setWeightEdgeTest() {
+        Graf<Integer> obj1 = new Graf<>();
+        obj1.addVert(1);
+        obj1.addVert(2);
+        obj1.addVert(3);
+        obj1.addEdge(1, 2, 3);
+        obj1.addEdge(1, 3, 6);
+        obj1.setWeightEdge(1, 3, 9);
+        Graf<Integer> obj = new Graf<>();
+        obj.addVert(1);
+        obj.addVert(2);
+        obj.addVert(3);
+        obj.addEdge(1, 2, 3);
+        obj.addEdge(1, 3, 9);
+        Assertions.assertEquals(obj1.adjMatrix(), obj.adjMatrix());
+    }
+
+    /**
+     * Testing getAllVert.
+     */
+
+    @Test
+    void getAllVertTest() {
+        Graf<String> obj1 = new Graf<>();
+        obj1.addVert("art");
+        obj1.addVert("moon");
+        obj1.addVert("roof");
+        List<String> ans = new ArrayList<>();
+        ans.add("roof");
+        ans.add("art");
+        ans.add("moon");
+        List<String> obj = obj1.getAllVert();
+        Assertions.assertEquals(obj, ans);
+    }
+
+    /**
+     * Testing getEdges.
+     */
+
+    @Test
+    void getEdgesTest() {
+        Graf<String> obj1 = new Graf<>();
+        obj1.addVert("art");
+        obj1.addVert("moon");
+        obj1.addVert("roof");
+        obj1.addEdge("art", "moon", 7);
+        obj1.addEdge("art", "roof", 23);
+        obj1.addEdge("moon", "moon", 1);
+        obj1.addEdge("roof", "art", 3);
+        List<String> obj2 = obj1.getEdges("art");
+        List<String> ans = new ArrayList<>();
+        ans.add("moon");
+        ans.add("roof");
+        Assertions.assertEquals(obj2, ans);
+        List<String> obj3 = obj1.getEdges("moon");
+        List<String> ans2 = new ArrayList<>();
+        ans2.add("moon");
+        Assertions.assertEquals(obj3, ans2);
+    }
+
+    /**
+     * Testing setVert.
+     */
+
+    @Test
+    void setVertTest() {
+        Graf<String> obj1 = new Graf<>();
+        obj1.addVert("art");
+        obj1.addVert("moon");
+        obj1.addVert("roof");
+        obj1.setVert("roof", "train");
+        Graf<String> obj = new Graf<>();
+        obj.addVert("art");
+        obj.addVert("moon");
+        obj.addVert("train");
+        Assertions.assertEquals(obj.incMatrix(), obj1.incMatrix());
+    }
+
+    /**
+     * Testing dijkstra.
+     */
+
+    @Test
+    void deikstraTest() {
+        Graf<Integer> obj = new Graf<>();
+        obj.addVert(1);
+        obj.addVert(2);
+        obj.addVert(3);
+        obj.addVert(4);
+        obj.addVert(5);
+        obj.addVert(6);
+        obj.addEdge(1, 2, 7);
+        obj.addEdge(1, 3, 9);
+        obj.addEdge(1, 6, 14);
+        obj.addEdge(2, 3, 10);
+        obj.addEdge(2, 4, 15);
+        obj.addEdge(3, 6, 2);
+        obj.addEdge(3, 4, 11);
+        obj.addEdge(4, 5, 6);
+        obj.addEdge(5, 6, 9);
+        Integer [] ans = obj.deikstra(1);
+        Integer [] act = new Integer[6];
+        act[0] = 0;
+        act[1] = 7;
+        act[2] = 9;
+        act[3] = 20;
+        act[4] = 26;
+        act[5] = 11;
+        Assertions.assertArrayEquals(ans, act);
+    }
+
+    /**
+     * Testing sort.
+     */
+
+    @Test
+    void sortTest() {
+        Graf<Integer> obj = new Graf<>();
+        obj.addVert(1);
+        obj.addVert(2);
+        obj.addVert(3);
+        obj.addVert(4);
+        obj.addVert(5);
+        obj.addVert(6);
+        obj.addEdge(1, 2, 1);
+        obj.addEdge(1, 3, 21);
+        obj.addEdge(1, 6, 14);
+        obj.addEdge(2, 3, 32);
+        obj.addEdge(2, 4, 15);
+        obj.addEdge(3, 6, 33);
+        obj.addEdge(3, 4, 11);
+        obj.addEdge(4, 5, 8);
+        obj.addEdge(5, 6, 9);
+        List<Integer> ans = obj.sortVert(1);
+        List<Integer> act = new ArrayList<>();
+        act.add(1);
+        act.add(2);
+        act.add(6);
+        act.add(4);
+        act.add(3);
+        act.add(5);
+        Assertions.assertEquals(ans, act);
+    }
+
+}
