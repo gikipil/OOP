cac6d66a4e2d472899b9146110eef5bd896e2f62
gikipil <frostcool1998@gmail.com>
2023-05-24 13:05:49 +0700

    fix reviewdog 6

--- Task_2_3_1/src/main/java/org/example/Game.java
+++ Task_2_3_1/src/main/java/org/example/Game.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
