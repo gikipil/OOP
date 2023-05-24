8e8e5b442e4a3395f41df8b1a27eabb367e680a4
gikipil <gikipil@users.noreply.github.com>
2023-05-24 05:18:06 +0000

    Deploying to gh-pages from @ gikipil/OOP@f09ee10427c30c47458afaa6f48096ba2c3e65aa 🚀

---
+++ Task_2_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Baker"},{"p":"org.example","l":"Courier"},{"p":"org.example","l":"Data"},{"p":"org.example","l":"Main"}]
