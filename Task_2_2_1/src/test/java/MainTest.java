ab1f7db6c5ee4653bbc6dce34732fcadb0c5a5f8
gikipil <frostcool1998@gmail.com>
2023-06-20 16:52:30 +0700

    fix reviewdog2

--- Task_2_2_1/src/test/java/MainTest.java
+++ Task_2_2_1/src/test/java/MainTest.java
@@ -1,11 +1,6 @@
 import org.example.Main;
 import org.junit.jupiter.api.Test;
 
-<<<<<<< HEAD
-=======
-import java.util.ArrayList;
-import java.util.List;
->>>>>>> 7aee954cc18de36bb7d84729f46998cc61f568ae
 
 /**
  * test.
