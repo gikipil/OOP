b667792b5cff347d36e6b32c2a891e82e5c78035
gikipil <gikipil@users.noreply.github.com>
2023-06-21 13:13:29 +0000

    Deploying to gh-pages from @ gikipil/OOP@40c505f6ce78054c822cbcda6b0d6024b9722894 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"PrimeNum"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"PrimeNum"},{"p":"org.example","l":"Read"}]
